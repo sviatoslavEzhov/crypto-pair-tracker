4b72b62aa7b3a714167c95ea9dd155aedba709d2
Sviatoslav Iezhov <sviatoslav.yezhov@gerchik.team>
2025-11-03 19:46:01 +0200

    feat: integrate Tailwind CSS with Vite in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@vueuse/nuxt']
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/image',
+        '@nuxt/ui',
+        '@vueuse/nuxt',
+    ],
+    css: ['./app/assets/css/main.css'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
 })
