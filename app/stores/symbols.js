35272c2a67f8800d578f0a064c1c13dae8003abe
Sviatoslav Iezhov <sviatoslav.yezhov@gerchik.team>
2025-11-04 05:36:48 +0200

    feat: update symbols store to handle API response status and improve formatting

--- app/stores/symbols.js
+++ app/stores/symbols.js
@@ -1,33 +1,33 @@
 import { SUCCESS } from '@/constants'
 
 export const useSymbolsStore = defineStore('symbols', () => {
-    const { $binanceApi } = useNuxtApp()
+  const { $binanceApi } = useNuxtApp()
 
-    const rawData = ref({})
-    const symbols = ref([])
-    const loaded = ref(false)
-    const loading = ref(false)
+  const rawData = ref({})
+  const symbols = ref([])
+  const loaded = ref(false)
+  const loading = ref(false)
 
-    const getSymbols = async () => {
-        loading.value = true
+  const getSymbols = async () => {
+    loading.value = true
 
-        try {
-            const { data } = await $binanceApi.symbols.read()
+    try {
+      const { data, status } = await $binanceApi.symbols.read()
 
-            if (data.value === SUCCESS) {
-                rawData.value = data.value
-                symbols.value = data.value.symbols
-                loaded.value = true
-            }
-        } catch (e) {
-            console.error(e)
-        } finally {
-            loading.value = false
-        }
+      if (status.value === SUCCESS) {
+        rawData.value = data.value
+        symbols.value = data.value.symbols
+        loaded.value = true
+      }
+    } catch (e) {
+      console.error(e)
+    } finally {
+      loading.value = false
     }
+  }
 
-    return {
-        symbols,
-        getSymbols
-    }
+  return {
+    symbols,
+    getSymbols
+  }
 })
